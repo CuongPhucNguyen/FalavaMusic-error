2d375bf7f7939ca497916471ef949621b617b248
oggy <vip12345k@gmail.com>
2022-09-05 18:50:17 +0700

    linking auth and database, set up data structure

--- Kimusic/Firestore/User.swift
+++ Kimusic/Firestore/User.swift
@@ -8,3 +8,24 @@
 import Foundation
 
 
+public struct AppUser: Codable {
+    let name: String
+    let photoURL: String?
+    let email: String?
+    let followers: Int64?
+    let following: Int64?
+    let friendList: [String]?
+    let playlists: [String : [String]]?
+    
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case photoURL
+        case email
+        case followers
+        case following
+        case friendList
+        case playlists
+    }
+
+}
